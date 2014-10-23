eea4a13f5266244a504fede1c7b9234f969cebc2
Ghost1227 <dgriffiths@section214.com>
2014-10-23 16:37:07 -0500

    Hide page slug

--- includes/admin/meta-boxes.php
+++ includes/admin/meta-boxes.php
@@ -12,16 +12,26 @@ if( ! defined( 'ABSPATH' ) ) exit;
 
 
 /**
+ * Remove the default submit meta box
+ *
+ * @since       1.0.0
+ * @return      void
+ */
+function timeapp_remove_meta_boxes() {
+    remove_meta_box( 'submitdiv', 'play', 'normal' );
+    remove_meta_box( 'submitdiv', 'purchaser', 'normal' );
+    remove_meta_box( 'submitdiv', 'artist', 'normal' );
+}
+add_action( 'admin_init', 'timeapp_remove_meta_boxes' );
+
+
+/**
  * Register new metaboxes
  *
  * @since       1.0.0
  * @return      void
  */
 function timeapp_add_meta_boxes() {
-    remove_meta_box( 'submitdiv', 'play', 'normal' );
-    remove_meta_box( 'submitdiv', 'purchaser', 'normal' );
-    remove_meta_box( 'submitdiv', 'artist', 'normal' );
-
     add_meta_box(
         'actions_top',
         __( 'Actions', 'timeapp' ),
