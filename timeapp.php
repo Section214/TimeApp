7f7b321ed017385df1536281b629a9d566d3b195
Ghost1227 <dgriffiths@section214.com>
2014-11-05 10:54:35 -0600

    Increment version

--- timeapp.php
+++ timeapp.php
@@ -3,7 +3,7 @@
  * Plugin Name:     TimeApp
  * Plugin URI:      http://ingroupconsulting.com/
  * Description:     Time Management's internal workflow tool
- * Version:         1.0.5
+ * Version:         1.0.6
  * Author:          Kiko Doran
  * Author URI:      http://ingroupconsulting.com/
  * Text Domain:     timeapp
@@ -95,7 +95,7 @@ if( ! class_exists( 'TimeApp' ) ) {
          */
         private function setup_constants() {
             // Plugin version
-            define( 'TIMEAPP_VER', '1.0.5' );
+            define( 'TIMEAPP_VER', '1.0.6' );
 
             // Plugin path
             define( 'TIMEAPP_DIR', plugin_dir_path( __FILE__ ) );
