c6a9c3ed68f3472406e12c28168c984f52030f65
Ghost1227 <dgriffiths@section214.com>
2015-08-25 23:56:56 -0500

    Version bump

--- timeapp.php
+++ timeapp.php
@@ -3,7 +3,7 @@
  * Plugin Name:     TimeApp
  * Plugin URI:      http://ingroupconsulting.com/
  * Description:     Time Management's internal workflow tool
- * Version:         2.0.1
+ * Version:         2.0.2
  * Author:          Kiko Doran
  * Author URI:      http://ingroupconsulting.com/
  * Text Domain:     timeapp
@@ -103,7 +103,7 @@ if( ! class_exists( 'TimeApp' ) ) {
          */
         private function setup_constants() {
             // Plugin version
-            define( 'TIMEAPP_VER', '2.0.1' );
+            define( 'TIMEAPP_VER', '2.0.2' );
 
             // Plugin path
             define( 'TIMEAPP_DIR', plugin_dir_path( __FILE__ ) );
